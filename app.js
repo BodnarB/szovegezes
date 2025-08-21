eacf12cca7fbd2c93aa71b1b1665eb502b4f9a64
BodnarB <bodnarbog94@gmail.com>
2025-08-21 21:46:00 +0200

    up motgp

--- app.js
+++ app.js
@@ -8,73 +8,35 @@ let mainTextCopBtn = document.querySelector('#main-text-copy')
 let teamLoadBtn = document.querySelector('.team-load')
 let storedTeam
 let eventValue = document.querySelector('#event')
-let currentEvent = 'Superpole versenyén'
+
 
 let csapatok = {
     home: {
-        90: "Dibusz Dénes",
-        99: "Gróf Dávid",
-        29: "Szécsi Gergő",
-        63: "Radnóti Dániel",
-        3: "Stefan Gartenmann",
-        21: "Botka Endre",
-        22: "Szalai Gábor",
-        27: "Ibrahim Cissé",
-        28: "Toon Raemaekers",
-        34: "Raul Gustavo",
-        44: "Ismail Aaneba",
-        54: "Kaján Norbert",
-        77: "Nagy Barnabás",
-        5: "Naby Keita",
-        15: "Mohammed Abu Fani",
-        16: "Kristoffer Zachariassen",
-        23: "Ötvös Bence",
-        25: "Cebrail Makreckis",
-        33: "Carlos Eduardo Lopes Cruz",
-        36: "Gavriel Gilad Kanichowsky",
-        47: "Callum O'Dowda",
-        64: "Tóth Alex",
-        66: "Júlio Romao",
-        80: "Habib Maiga",
-        88: "Philippe Rommens",
-        7: "Daniel Arzani",
-        8: "Aleksandar Pesic",
-        10: "Jonathan Levi",
-        17: "Edgar Szevikjan",
-        19: "Varga Barnabás",
-        20: "Adama Traoré",
-        24: "Tosin Kehinde",
-        30: "Gruber Zsombor",
-        32: "Aleksandar Cirkovic",
-        74: "Szabó Szilárd",
-        75: "Lenny Joseph",
-        76: "Lisztes Krisztián"
+        1: "A spanyol Jorge Martín, az Aprilia Racing versenyzője",
+        5: "A francia Johann Zarco, a Castrol Honda LCR versenyzője",
+        10: "Az olasz Luca Marini, a Honda HRC Castrol versenyzője",
+        12: "A spanyol Maverick Vinales, a Red Bull KTM Tech3 versenyzője",
+        20: "A francia Fabio Quartararo, a Monster Energy Yamaha MotoGP Team versenyzője",
+        21: "Az olasz Franco Morbidelli, a Pertamina Enduro VR46 Racing Team versenyzője",
+        23: "Az olasz Enea Bastianini, a Red Bull KTM Tech3 versenyzője",
+        25: "A spanyol Raúl Fernández, a Trackhouse MotoGP Team versenyzője",
+        33: "A dél-afrikai Brad Binder, a Red Bull KTM Factory Racing versenyzője",
+        35: "A thai Somkiat Chantra, az Idemitsu Honda LCR versenyzője",
+        36: "A spanyol Joan Mir, a Honda HRC Castrol versenyzője",
+        37: "A spanyol Pedro Acosta, a Red Bull KTM Factory Racing versenyzője",
+        42: "A spanyol Álex Rins, a Monster Energy Yamaha MotoGP Team versenyzője",
+        43: "Az ausztrál Jack Miller, a Prima Pramac Yamaha MotoGP versenyzője",
+        49: "Az olasz Fabio Di Giannantonio, a Pertamina Enduro VR46 Racing Team versenyzője",
+        54: "A spanyol Fermín Aldeguer, a BK8 Gresini Racing MotoGP versenyzője",
+        63: "Az olasz Francesco Bagnaia, a Ducati Lenovo Team versenyzője",
+        72: "Az olasz Marco Bezzecchi, az Aprilia Racing versenyzője",
+        73: "A spanyol Álex Márquez, a BK8 Gresini Racing MotoGP versenyzője",
+        79: "A japán Ai Ogura, a Trackhouse MotoGP Team versenyzője",
+        88: "A portugál Miguel Oliveira, a Prima Pramac Yamaha MotoGP versenyzője",
+        93: "A spanyol Marc Márquez, a Ducati Lenovo Team versenyzője"
     },
     guest: {
-        1: "Sahrudin Magomedalijev",
-        97: "Fabijan Buntic",
-        99: "Mateusz Kochalski",
-        2: "Matheus Silva",
-        3: "Samy Mmaee",
-        13: "Bahlul Mustafazade",
-        18: "Dani Bolt",
-        26: "Amin Rzayev",
-        30: "Abbas Hüseynov",
-        44: "Elvin Jafarguliyev",
-        55: "Badavi Hüseynov",
-        81: "Kevin Medina",
-        6: "Chris Kouakou",
-        8: "Marko Jankovic",
-        10: "Abdellah Zoubir",
-        15: "Leandro Andrade",
-        20: "Kady",
-        21: "Olekszij Kascsuk",
-        27: "Tural Bayramov",
-        32: "Hikmat Jabrayilzade",
-        35: "Pedro Bicalho",
-        11: "Emmanuel Addai",
-        22: "Musa Gurbanli",
-        90: "Nariman Akhundzade"
+
     }
 }
 
@@ -92,12 +54,12 @@ guestTeamText.value = csapatObjToText(csapatok.guest)
 
 
 // // ideiglenes SBK-ra
-szovegTextbox.value = `X, a Ferencváros (b) és Y, a Qarabag játékosa a labdarúgó Bajnokok Ligája selejtezőjének negyedik fordulójában játszott Ferencvárosi TC - Qarabag mérkőzésen a Groupama Arénában 2025. augusztus 19-én. `
+szovegTextbox.value = `X a MotoGP Magyar Nagydíj első szabadedzésén a balatonfőkajári Balaton Park Circuit versenypályán 2025. augusztus 22-én. `
 
-// eventValue.addEventListener('change', () => {
-//     currentEvent = eventValue.options[eventValue.selectedIndex].text
-//     szovegTextbox.value = ` a gyorsaságimotoros Superbike-világbajnokság magyarországi futamának ${currentEvent} a balatonfőkajári Balaton Park Circuit versenypályán 2025. július 27-én.`
-// })
+eventValue.addEventListener('change', () => {
+    currentEvent = eventValue.options[eventValue.selectedIndex].text
+    szovegTextbox.value = ` X a MotoGP Magyar Nagydíj ${currentEvent} a balatonfőkajári Balaton Park Circuit versenypályán 2025. augusztus 22-én.`
+})
 // // ideiglenes SBK-ra
 
 
@@ -127,13 +89,6 @@ function parseCsapat(szoveg) {
 
     return csapat
 }
-
-// ideiglenes SBK-ra
-// teamLoadBtn.addEventListener('click', () => {
-// homeTeamText.value = storedTeam.join('\n')
-// })
-// ideiglenes SBK-ra
-
 
 homeTeamText.addEventListener('input', () => {
     csapatok.home = parseCsapat(homeTeamText.value)
