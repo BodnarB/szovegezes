6148ec78b9df97a1c0741345b635d1b801bfa14e
BodnarB <bodnarbog94@gmail.com>
2025-09-20 08:45:22 +0200

    up kezi

--- app.js
+++ app.js
@@ -12,58 +12,54 @@ let eventValue = document.querySelector('#event')
 
 let csapatok = {
     home: {
-        1: "Milica Kostic",
-        21: "Major Janka",
-        22: "Barti Luca",
-        4: "Túróczy Anna",
-        5: "Brigita Goder",
-        6: "Andjela Frajtovic",
-        8: "Nyul Petra",
-        13: "Gagyor Zille",
-        20: "Ott Eszter",
-        23: "Németh Adél",
-        3: "Hanász Vanda",
-        7: "Zágonyi Barbara",
-        11: "Milagros Diaz",
-        14: "Fenyvesi Evelin",
-        19: "Kovács Petra",
-        27: "Jadyn Edwards",
-        33: "Kovács Eszter",
-        45: "Bozsik Linett",
-        88: "Czellér Dorottya",
-        10: "Diószegi Fanni",
-        18: "Czinege Eszter",
-        24: "Alesia Garcia",
-        25: "Nagy Viktória",
-        32: "Gégény Petra",
-        48: "Nagy Vanessza",
-        58: "Vlada Kubassova"
+        2: "Szmolek Apollónia",
+        4: "Tóvizi Petra",
+        5: "Csíkos Luca",
+        9: "Hársfalvi Júlia",
+        10: "Faragó Lea",
+        11: "Albek Anna",
+        17: "Kukely Anna",
+        18: "Kovács Anett",
+        19: "Márton Gréta",
+        24: "Töpfner Alexandra",
+        30: "Szemerey Zsófi",
+        38: "Vámos Petra",
+        42: "Klujber Katrin",
+        58: "Bordás Réka",
+        59: "Kuczora Csenge",
+        61: "Janurik Kinga",
+        71: "Petrus Mirtill",
+        74: "Bukovszky Anna",
+        77: "Simon Petra",
+        88: "Papp Nikoletta",
+        98: "Grosch Vivien"
     },
     guest: {
-        1: "Tove Enblom",
-        24: "Pia Grinde-Hansen",
-        31: "Thiril Erichsen",
-        2: "Sara Horte",
-        3: "Michaela Kovacs",
-        5: "Selma Pettersen",
-        28: "Arna Eiriksdottir",
-        6: "Tilde Lindwall",
-        7: "Naina Inauen",
-        8: "Linn Vickius",
-        10: "Olaug Tvedten",
-        15: "Ylinn Tennebo",
-        16: "Ronja Arnesen",
-        17: "Ronja Aronsson",
-        19: "Saedis Heidarsdottir",
-        23: "Eline Hegg",
-        25: "Synne Hansen",
-        29: "Marika Bergman-Lundin",
-        30: "Stine Brekken",
-        9: "Elise Thorsnes",
-        11: "Marie Preus",
-        14: "Mawa Sesay",
-        21: "Karina Saevik",
-        22: "Tommine Enger"
+        1: "Johanna Bundsen",
+        2: "Clara Lerby",
+        3: "Nina Koppang",
+        7: "Linn Blohm",
+        8: "Jamina Roberts",
+        9: "Melissa Petrén",
+        11: "Tyra Axnér",
+        12: "Filippa Idéhn",
+        16: "Jessica Ryde",
+        19: "Anna Lagerquist",
+        21: "Evelina Eriksson",
+        23: "Emma Lindqvist",
+        24: "Nathalie Hagman",
+        27: "Thea Kylberg",
+        29: "Kristin Thorleifsdóttir",
+        33: "Isabel van Kerkvoorde",
+        35: "Sofia Hvenfelt",
+        36: "Sofie Börjesson",
+        38: "Elin Hansson",
+        40: "Vilma Matthijs Holmberg",
+        42: "Jenny Carlson",
+        49: "Olivia Löfqvist",
+        52: "Charité Mumbongo",
+        55: "Clara Petersson Bergsten",
+        97: "Linn Hansson"
     }
 }
 
@@ -81,12 +77,12 @@ guestTeamText.value = csapatObjToText(csapatok.guest)
 
 
 // // ideiglenes SBK-ra
-szovegTextbox.value = `X, a Ferencváros (j) és Y, a Valerenga játékosa a női labdarúgó Bajnokok Ligája selejtezőjének harmadik fordulójában játszott FTC-Telekom - Valerenga FC mérkőzésen a III. kerületi TVE Sporttelepen 2025. szeptember 18-án. `
+szovegTextbox.value = `X (b) és a svéd Y a Magyarország - Svédország női kézilabda felkészülési mérkőzésen a Tatabányai Multifunkciós Csarnokban 2025. szeptember 20-án. `
 
-eventValue.addEventListener('change', () => {
-    currentEvent = eventValue.options[eventValue.selectedIndex].text
-    szovegTextbox.value = `X a MotoGP Magyar Nagydíj ${currentEvent} a balatonfőkajári gyorsaságimotoros-világbajnokságon, a Balaton Park Circuit versenypályán 2025. augusztus 24-én.`
-})
+// eventValue.addEventListener('change', () => {
+//     currentEvent = eventValue.options[eventValue.selectedIndex].text
+//     szovegTextbox.value = `X a MotoGP Magyar Nagydíj ${currentEvent} a balatonfőkajári gyorsaságimotoros-világbajnokságon, a Balaton Park Circuit versenypályán 2025. augusztus 24-én.`
+// })
 // // ideiglenes SBK-ra
 
 
